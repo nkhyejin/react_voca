886eca7c7e4e0ed4972284dc9e6c517a81c150da
남궁혜진 <nkhj0404@gmail.com>
2022-10-01 19:12:08 +0900

    잘못된 경로 처리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DayList from "./component/DayList";
 import Header from "./component/Header";
 import Day from "./component/Day";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
         </Routes>
       </div>
     </BrowserRouter>
