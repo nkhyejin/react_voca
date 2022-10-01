76b88a79bc37e693f591824592a9e6ae73b1f64d
남궁혜진 <nkhj0404@gmail.com>
2022-10-01 17:34:51 +0900

    Daylist 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import DayList from "./component/DayList";
 import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <DayList></DayList>
     </div>
   );
 }
---
+++ src/component/DayList.js
@@ -0,0 +1,15 @@
+import dummy from "../db/data.json";
+
+const DayList = () => {
+  console.log(dummy);
+  console.log(dummy.days);
+  return (
+    <ul className="list_day">
+      {dummy.days.map((day) => (
+        <li key={day.id}>Day {day.day}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default DayList;
