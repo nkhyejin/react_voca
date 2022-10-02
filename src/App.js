42f6afc44f1fc1c190499cfe348c91e3ceb4cfd9
남궁혜진 <nkhj0404@gmail.com>
2022-10-02 13:51:57 +0900

    단어 추가 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./component/Header";
 import Day from "./component/Day";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
           <Route path="*" element={<EmptyPage />} />
         </Routes>
       </div>
